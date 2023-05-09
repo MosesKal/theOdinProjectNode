16e16aa0b169648826613e8c92cfff4c89f17cda
MosesK <mosesziongo@gmail.com>
2023-05-10 01:28:45 +0200

    ajout du routage vers different page

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 import http from "http";
+import fs from "node:fs";
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
+    if (req.url === "/") {
+      fs.readFile("index.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end;
+      });
+    } else if (req.url === "/about") {
+      fs.readFile("about.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end;
+      });
+    } else if (req.url === "/contact-me") {
+      fs.readFile("about.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end;
+      });
+    } else {
+      fs.readFile("404.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end;
+      });
+    }
   })
-  .listen(8080);
+  .listen(8080, () => {
+    console.log("Listening on port 8080");
+  });
